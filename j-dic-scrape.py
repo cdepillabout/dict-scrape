c1d30fda3dedaef6c86094b281d01a85da90dbf6
(cdep) illabout <cdep.illabout@gmail.com>
2012-05-31 10:09:03 -0400

    Modified to also make progressive work with new classes.  Now need to add comments.

--- j-dic-scrape.py
+++ j-dic-scrape.py
@@ -10,7 +10,6 @@ from StringIO import StringIO
 
 #from IPython import embed ; embed()
 
-progressive = {'dtype': '3', 'dname': '2na'}
 
 hiragana = """
 あいうえお
@@ -138,11 +137,37 @@ class Result(object):
         return self.result_eng_defs
 
 class Dictionary(object):
+    DAIJIRIN_TYPE = 0
+    DAIJISEN_TYPE = 1
+    NEW_CENTURY_TYPE = 2
+    PROGRESSIVE_TYPE = 3
+
     def __init__(self):
         pass
 
     def lookup(self, word_kanji, word_kana):
         tree = self.create_page_tree(word_kanji, word_kana)
+
+        # make sure there is an entry
+        result = etree.tostring(tree, pretty_print=False, method="html", encoding='UTF-8')
+        word_not_found_string = \
+                '<p><em>%s %s</em>に一致する情報はみつかりませんでした。</p>' % \
+                (word_kanji, word_kana)
+        word_not_found_string_no_space = \
+                '<p><em>%s%s</em>に一致する情報はみつかりませんでした。</p>' % \
+                (word_kanji, word_kana)
+
+        # return None if we can't find a definition
+        if word_not_found_string in result or word_not_found_string_no_space in result:
+            #print("NO DEFINITION FOUND")
+            return None
+
+        # make sure this is the new century and not the progressive definition
+        if self.dic_type == Dictionary.NEW_CENTURY_TYPE:
+            if '<span class="dic-zero">ニューセンチュリー和英辞典</span>' in result:
+                #print("NO DEFINITION FROM NEW CENTURY")
+                return None
+
         kanji, kana, accent = self.parse_heading(tree)
         jap_defs_sentences, eng_defs_sentences = self.parse_definition(tree)
         return Result(kanji, kana, accent, jap_defs_sentences, eng_defs_sentences)
@@ -150,7 +175,7 @@ class Dictionary(object):
     def create_page_tree(self, word_kanji, word_kana):
         search = "%s　%s" % (word_kanji, word_kana)
         params = {'p': search, 'enc': "UTF-8", 'stype': 1,
-                'dtype': self.dtype, 'dname': self.dname}
+                'dtype': self.dtype_search_param, 'dname': self.dname_search_param}
         encoded_params = urllib.urlencode(params)
         page = urllib.urlopen("http://dic.yahoo.co.jp/dsearch?%s" % encoded_params)
         page_string = page.read()
@@ -169,11 +194,16 @@ class Dictionary(object):
     def dic_name(self):
         return self.dictionary_name
 
+    @property
+    def dic_type(self):
+        return self.dictionary_type
+
 class DaijirinDictionary(Dictionary):
     def __init__(self):
         self.dictionary_name = "Yahoo's Daijirin (大辞林)"
-        self.dtype = '0'
-        self.dname = '0ss'
+        self.dictionary_type = Dictionary.DAIJIRIN_TYPE
+        self.dtype_search_param = '0'
+        self.dname_search_param = '0ss'
         #super(DaijirinDictionary, self).__init__()
 
     def parse_heading(self, tree):
@@ -234,11 +264,36 @@ class DaijirinDictionary(Dictionary):
 
         return jap_defs, None
 
+class DaijisenDictionary(DaijirinDictionary):
+    def __init__(self):
+        self.dictionary_name = "Yahoo's Daijisen (大辞泉)"
+        self.dictionary_type = Dictionary.DAIJISEN_TYPE
+        self.dtype_search_param = '0'
+        self.dname_search_param = '0na'
+        #super(DaijirinDictionary, self).__init__()
+
+    def parse_definition(self, tree):
+        defs = tree.xpath("//table[@class='d-detail']/tr/td")[0]
+        result = etree.tostring(defs, pretty_print=False, method="html", encoding='UTF-8')
+        jap_defs = []
+        matches = re.findall("<b>[１|２|３|４|５|６|７|８|９|０]+</b> (.*?)<br>", result)
+        if matches:
+            for m in matches:
+                jap_defs.append(Definition(m, None, None))
+        else:
+            result = re.sub("^<td>", "", result)
+            result = re.sub("<br></td>.*$", "", result)
+            result = result.strip()
+            jap_defs.append(Definition(result, None, None))
+
+        return jap_defs, None
+
 class NewCenturyDictionary(DaijirinDictionary):
     def __init__(self):
-        self.dictionary_name = "Yahoo's New Century (ニューセンチュリー和英辞典)"
-        self.dtype = '3'
-        self.dname = '2ss'
+        self.dictionary_name = "Yahoo's New Century Dictionary (ニューセンチュリー和英辞典)"
+        self.dictionary_type = Dictionary.NEW_CENTURY_TYPE
+        self.dtype_search_param = '3'
+        self.dname_search_param = '2ss'
 
     def parse_definition(self, tree):
         def_elems = tree.xpath("//table[@class='d-detail']/tr/td")[0]
@@ -302,144 +357,66 @@ class NewCenturyDictionary(DaijirinDictionary):
 
         return None, definitions
 
-class DaijisenDictionary(DaijirinDictionary):
+class ProgressiveDictionary(DaijirinDictionary):
     def __init__(self):
-        self.dictionary_name = "Yahoo's Daijisen (大辞泉)"
-        self.dtype = '0'
-        self.dname = '0na'
-        #super(DaijirinDictionary, self).__init__()
+        self.dictionary_name = "Yahoo's Progressive Dictionary (プログレッシブ和英中辞典)"
+        self.dictionary_type = Dictionary.PROGRESSIVE_TYPE
+        self.dtype_search_param = '3'
+        self.dname_search_param = '2na'
 
     def parse_definition(self, tree):
         defs = tree.xpath("//table[@class='d-detail']/tr/td")[0]
         result = etree.tostring(defs, pretty_print=False, method="html", encoding='UTF-8')
-        jap_defs = []
-        matches = re.findall("<b>[１|２|３|４|５|６|７|８|９|０]+</b> (.*?)<br>", result)
+
+        definitions = []
+
+        multiple_defs = True
+
+        # do we have multiple definitions?
+        matches = re.search('^<td>\n<b>1</b> 〔', result)
         if matches:
-            for m in matches:
-                jap_defs.append(Definition(m, None, None))
+            # split the page into pieces for each definition
+            splits = re.split('(<b>[1|2|3|4|5|6|7|8|9|0]+</b> 〔)', result)
+            # make sure we have an odd number of splits
+            assert(len(splits) % 2 == 1)
+            # throw away the first split because it's useless information
+            splits = splits[1:]
+            # combine the following splits
+            # This is stupidly complicated.  Basically we have a list like
+            # ["ab", "cd", "ef", "gh", "hi", "jk"] and we want to combine it
+            # to make a list like ["abcd", "efgh", "hijk"]
+            splits = ["%s%s" % (splits[i], splits[i+1]) for i in range(0, len(splits), 2)]
         else:
-            result = re.sub("^<td>", "", result)
-            result = re.sub("<br></td>.*$", "", result)
-            result = result.strip()
-            jap_defs.append(Definition(result, None, None))
+            splits = [result]
+            multiple_defs = False
 
-        return jap_defs, None
-
-
-def progressive_heading(tree):
-    # THIS IS THE SAME AS NEW_CENTURY HEADING
-    div = tree.xpath("//div[@class='title-keyword']")[0]
-    #result = etree.tostring(div, pretty_print=True, method="html", encoding='UTF-8')
-    #print(result)
-    heading = div.getchildren()[0]
-    children = heading.getchildren()
-    #from IPython import embed ; embed()
-
-    result = heading.xpath("text()")
-    result = "".join(result)
-    #print(result)
-
-    # THIS IS DIFFERENT!!! this takes out the non-greedy match
-    m = re.search("^(.*)[ | |【|［|〔]".decode("utf-8"), result)
-    if m:
-        result_kana = m.group(1)
-    else:
-        # we didn't get a match, so the word we are trying to find
-        # should just be the entire string
-        result_kana = result
-
-    # THIS STRIPS< THIS IS DIFFERENT
-    result_kana = result_kana.strip()
-
-    # this is just in case we don't get any kanji later
-    result_word = result_kana
-
-    m = re.search("【(.*)】$".decode('utf-8'), result)
-    if m:
-        result_word = m.group(1)
-
-    print("%s: \"%s\"" %
-            (result_word.encode('utf-8'), result_kana.encode('utf-8')))
-
-def progressive_definition(tree):
-    definitions = tree.xpath("//table[@class='d-detail']/tr/td")[0]
-    result = etree.tostring(definitions, pretty_print=False, method="html", encoding='UTF-8')
-
-    multiple_defs = True
-
-    # do we have multiple definitions?
-    matches = re.search('^<td>\n<b>1</b> 〔', result)
-    if matches:
-        # split the page into pieces for each definition
-        splits = re.split('(<b>[1|2|3|4|5|6|7|8|9|0]+</b> 〔)', result)
-        # make sure we have an odd number of splits
-        assert(len(splits) % 2 == 1)
-        # throw away the first split because it's useless information
-        splits = splits[1:]
-        # combine the following splits
-        # This is stupidly complicated.  Basically we have a list like
-        # ["ab", "cd", "ef", "gh", "hi", "jk"] and we want to combine it
-        # to make a list like ["abcd", "efgh", "hijk"]
-        splits = ["%s%s" % (splits[i], splits[i+1]) for i in range(0, len(splits), 2)]
-        """
-        for s in splits:
-            print "M: %s" % s
-            print
-            print
-            print
-        print("multiple definitions")
-        """
-    else:
-        splits = [result]
-        multiple_defs = False
-
-    definition_counter = 1
-    for splt in splits:
-        print("DEFINTION %d********************************" % definition_counter)
-        definition_counter += 1
-
-        # find english definition
-        #print splt
-        # make sure not to match on the initial character telling whether
-        # it is a noun,verb, etc
-        if multiple_defs == True:
-            match = re.search('<b>[1|2|3|4|5|6|7|8|9|0]+</b> 〔(.*?)<br><br>', splt)
-            if match:
-                print("(ENGLISH) 〔%s" % match.group(1))
-        else:
-            match = re.search('^<td>\n(.*?)<br>', splt)
-            if match:
-                if not match.group(1).startswith("[例文]"):
-                    print("(ENGLISH) %s" % match.group(1))
+        for splt in splits:
+            # find english definition
+            english_def = None
+            # make sure not to match on the initial character telling whether
+            # it is a noun,verb, etc
+            if multiple_defs == True:
+                match = re.search('<b>[1|2|3|4|5|6|7|8|9|0]+</b> 〔(.*?)<br><br>', splt)
+                if match:
+                    english_def = "〔%s" % match.group(1)
+            else:
+                match = re.search('^<td>\n(.*?)<br>', splt)
+                if match:
+                    if not match.group(1).startswith("[例文]"):
+                        english_def =  match.group(1)
 
 
-        # find example sentences
-        matches = re.findall('<td><small><font color="#008800"><b>(.*?)</b></font><br><font color="#666666">(.*?)</font></small></td>', splt)
-        counter = 1
-        print("EXAMPLE SENTENCES:")
-        if matches:
-            for m in matches:
-                print("（%d）%s (%s)" % (counter, m[0], m[1]))
-                counter += 1
+            # find example sentences
+            example_sentences = []
+            matches = re.findall('<td><small><font color="#008800"><b>(.*?)</b></font><br><font color="#666666">(.*?)</font></small></td>', splt)
+            if matches:
+                for m in matches:
+                    example_sentences.append(ExampleSentence(m[0], m[1]))
 
-def check_new_century(word_kanji, word_kana):
-    tree = create_page_tree(word_kanji, word_kana, new_century['dtype'], new_century['dname'])
-    # make sure there is an entry
-    result = etree.tostring(tree, pretty_print=False, method="html", encoding='UTF-8')
-    word_not_found_string = '<p><em>%s %s</em>に一致する情報はみつかりませんでした。</p>' % \
-            (word_kanji, word_kana)
-    word_not_found_string_no_space = \
-            '<p><em>%s%s</em>に一致する情報はみつかりませんでした。</p>' % \
-            (word_kanji, word_kana)
-    if word_not_found_string in result or word_not_found_string_no_space in result:
-        print("NO DEFINITION FOUND")
-        return
-    # make sure this is the new century and not the progressive definition
-    if '<span class="dic-zero">ニューセンチュリー和英辞典</span>' in result:
-        print("NO DEFINITION FROM NEW CENTURY")
-        return
-    new_century_heading(tree)
-    new_century_definition(tree)
+            definitions.append(Definition(english_def, example_sentences, None))
+
+        return None, definitions
+
 
 def check_progressive(word_kanji, word_kana):
     tree = create_page_tree(word_kanji, word_kana, progressive['dtype'], progressive['dname'])
@@ -460,17 +437,17 @@ if __name__ == '__main__':
     words = [
             ('強迫', 'きょうはく'),
             ('面白い', 'おもしろい'),
-            #('赤し', 'あかし'),
+            ('赤し', 'あかし'),
             ('うっとり', ''),
             ('バリカン', ''),
-            #('コンピエーニュ', ''),
+            ('コンピエーニュ', ''),
             ('蜥蜴', 'とかげ'),
             ('らくだ', '駱駝'),
             ('成り済ます', 'なりすます'),
             ('行く', 'いく'),
             ('が', ''),
             ('遊ぶ', 'あそぶ'),
-            #('遊ぶ', 'あすぶ'),
+            ('遊ぶ', 'あすぶ'),
             ]
 
     """
@@ -484,11 +461,16 @@ if __name__ == '__main__':
     daijirin_dic = DaijirinDictionary()
     daijisen_dic = DaijisenDictionary()
     new_century_dic = NewCenturyDictionary()
+    progressive_dic = ProgressiveDictionary()
 
     def print_all_defs(defs):
         for i in range(len(defs)):
             d = ""
-            print("(%2d) %s" % (i, defs[i].definition))
+            if defs[i].definition:
+                definition = defs[i].definition
+            else:
+                definition = "NO DEFINITION AVAILABLE"
+            print("(%2d) %s" % (i, definition))
             for e in defs[i].example_sentences:
                 print("    - %s" % e.jap_sentence)
                 if e.eng_trans:
@@ -496,9 +478,14 @@ if __name__ == '__main__':
 
 
     for word in words:
-        for d in [daijirin_dic, daijisen_dic, new_century_dic]:
-            result = d.lookup(word[0], word[1])
+        for d in [daijirin_dic, daijisen_dic, new_century_dic, progressive_dic]:
             print ("FROM %s" % d.dic_name)
+            result = d.lookup(word[0], word[1])
+            if not result:
+                print("NO RESULT FOUND!!!!!")
+                print
+                continue
+
             print ("%s (%s) %s:" % (result.kanji, result.kana, result.accent))
             if result.jap_defs:
                 print("jap defs:")
