67a4ee9d7ea3e7be98a155312f24e194775d9877
(cdep) illabout <cdep.illabout@gmail.com>
2012-06-08 09:10:11 -0400

    Updated test word manager.

--- test/support/words/manage_test_words.py
+++ test/support/words/manage_test_words.py
@@ -5,6 +5,7 @@ import argparse
 import codecs
 import json
 import operator
+import os
 import os.path
 import sys
 import traceback
@@ -16,9 +17,114 @@ from jdicscrape import DaijirinDictionary, DaijisenDictionary, \
         ProgressiveDictionary, NewCenturyDictionary, ExampleSentence, \
         Definition, Result
 
+dics = [DaijirinDictionary(),
+        DaijisenDictionary(),
+        ProgressiveDictionary(),
+        NewCenturyDictionary()]
+
 def die(string):
     print(u'ERROR! %s' % string)
     sys.exit(1)
+
+words_db_rel_path = os.path.join(os.path.dirname(__file__), "words.db")
+words_db_abs_path = os.path.abspath(words_db_rel_path)
+if not (os.path.exists(words_db_abs_path)):
+    die("words.db does not exist! Something is wrong!")
+
+def get_words_from_wordsdb():
+    """Get all the words from the words.db."""
+    words = []
+
+    # read in current words
+    with codecs.open(words_db_abs_path, 'r', 'utf8') as f:
+        lines = f.read().split('\n')
+    for l in lines:
+        if l != '':
+            kana, kanji = l.split()
+            words.append(tuple([kana, kanji]))
+
+    return words
+
+def sanity_check():
+    """Make sure our words.db and actual words that have been downloaded match up."""
+    words = get_words_from_wordsdb()
+
+    # TODO: make sure every word in words is unique
+    for w in words:
+        kana = w[0]
+        kanji = w[1]
+        existing_words = [word for word in words if word[0] == kana and word[1] == kanji]
+        if len(existing_words) > 1:
+            die("%s (%s) exists more than once in words.db" % (kanji, kana))
+
+    # make sure there is a file for each of our words in each dictionary directory
+    for dic in dics:
+        dirname = dic.short_dic_name
+        rel_path = os.path.join(os.path.dirname(__file__), dic.short_dic_name)
+        abs_path = os.path.abspath(rel_path)
+
+        for w in words:
+            kana = w[0]
+            kanji = w[1]
+
+            html_filename = u'%s_%s.html' % (kana, kanji)
+            result_filename = u'%s_%s.result.json' % (kana, kanji)
+
+            # absolute paths to the two files we will use
+            html_file_abs_path = os.path.join(abs_path, html_filename)
+            result_file_abs_path = os.path.join(abs_path, result_filename)
+
+            # relative paths to the two files we will use to print out
+            html_file_rel_path = os.path.join(rel_path, html_filename)
+            result_file_rel_path = os.path.join(rel_path, result_filename)
+
+            if not os.path.exists(html_file_abs_path):
+                die(u'%s (%s) is in words.db, but "%s" does not exist.' %
+                        (kanji, kana, html_file_rel_path))
+
+            if not os.path.exists(result_file_abs_path):
+                die(u'%s (%s) is in words.db, but "%s" does not exist.' %
+                        (kanji, kana, result_file_rel_path))
+
+        for entry in os.listdir(abs_path):
+            entry = entry.decode('utf8')
+            entry_rel_path = os.path.join(rel_path, entry)
+            try:
+                # rip off the extension
+                name = entry.split('.')[0]
+                # get kanji and kana
+                kana, kanji = name.split('_')
+            except:
+                die("Inappropriate file %s" % entry_rel_path)
+
+            # make sure kanji and kana are in words.db
+            existing_words = [word for word in words if word[0] == kana and word[1] == kanji]
+            if len(existing_words) < 1:
+                die(u'%s (%s) from %s does not exist in words.db' %
+                        (kanji, kana, entry_rel_path))
+
+def add_word_to_wordsdb(word_kanji, word_kana):
+    """Update the words.db with word_kanji and word_kana."""
+    words = get_words_from_wordsdb()
+
+    # make sure our new word is not in words.db
+    for word in words:
+        if word_kana == word[0] and word_kanji == word[1]:
+            die("%s (%s) already exists in words.db." % (word_kanji, word_kana))
+
+    # add our new word
+    words.append(tuple([word_kana, word_kanji]))
+
+    # sort words
+    words = sorted(words, key=operator.itemgetter(1,0))
+    for word in words:
+        print("%s (%s)" % (word[1], word[0]))
+
+    # write sorted word list words
+    with codecs.open(words_db_abs_path, 'w', 'utf8') as f:
+        for word in words:
+            f.write(u'%s %s\n' % (word[0], word[1]))
+
 
 def addword(word_kanji, word_kana):
     # make sure we get unicode words
@@ -26,10 +132,6 @@ def addword(word_kanji, word_kana):
     assert(type(word_kana) == type(unicode()))
 
     # get the page
-    dics = [DaijirinDictionary(),
-            DaijisenDictionary(),
-            ProgressiveDictionary(),
-            NewCenturyDictionary()]
 
     add_word_to_wordsdb(word_kanji, word_kana)
 
@@ -91,99 +193,25 @@ def addword(word_kanji, word_kana):
                 json.dump(jsonable, f, encoding='utf8', sort_keys=True,
                         indent=4, ensure_ascii=False)
 
-def add_word_to_wordsdb(word_kanji, word_kana):
-    # Update the words.db with the current word we just added.
-    words_db_rel_path = os.path.join(os.path.dirname(__file__), "words.db")
-    words_db_abs_path = os.path.abspath(words_db_rel_path)
-    if not (os.path.exists(words_db_abs_path)):
-        die("words.db does not exist! Something is wrong!")
-
-    words = []
-
-    # read in current words
-    with codecs.open(words_db_abs_path, 'r', 'utf8') as f:
-        lines = f.read().split('\n')
-    for l in lines:
-        if l != '':
-            kana, kanji = l.split()
-            words.append(tuple([kana, kanji]))
-
-    # make sure our new word is not in words.db
-    for word in words:
-        if word_kana == word[0] and word_kanji == word[1]:
-            die("%s (%s) already exists in words.db." % (word_kanji, word_kana))
-
-    # add our new word
-    words.append(tuple([word_kana, word_kanji]))
-
-    # sort words
-    words = sorted(words, key=operator.itemgetter(1,0))
-    for word in words:
-        print("%s (%s)" % (word[1], word[0]))
-
-    # write sorted word list words
-    with codecs.open(words_db_abs_path, 'w', 'utf8') as f:
-        for word in words:
-            f.write(u'%s %s\n' % (word[0], word[1]))
 
 def main():
-    """
-    parser = argparse.ArgumentParser(description="Install the current kernel.")
-    parser.add_argument('--make', '-m', action='store_true',
-            help="run `make`")
-    parser.add_argument('--make-install-modules', '-i', action='store_true',
-            help="run `make modules_install`")
-
-    parser.add_argument('--install-kernel', '-k', action='store_true',
-            help="install kernel image to /boot")
-
-    parser.add_argument('--ssh', '-s', action='store', metavar='REMOTE_MACHINE',
-            help="use ssh to copy kernel to REMOTE_MACHINE")
-
-    parser.add_argument('--jobs', '-j', action='store', default=get_default_jobs(), type=int,
-            help="specify the number of jobs (default %s)" % get_default_jobs())
-
-    parser.add_argument('--ctags', '-t', action='store_true',
-            help="run `make tags`")
-
-    make_config_group = parser.add_mutually_exclusive_group()
-    make_config_group.add_argument('--make-menuconfig', '-c', action='store_true',
-            help="run `make menuconfig` first")
-    make_config_group.add_argument('--make-oldconfig', '-o', action='store_true',
-            help="run `make oldconfig` first")
-
-    build_dir_group = parser.add_mutually_exclusive_group()
-    build_dir_group.add_argument('--build-dir', '-b', action='store',
-            help="use a separate build directory, defaults to \"build/\"",
-            default="./build")
-    build_dir_group.add_argument('--no-build-dir', '-n', action='store_true',
-            help="don't use a separate build directory")
-
-    ccache_group = parser.add_mutually_exclusive_group()
-    ccache_group.add_argument('--ccache', '-f', action='store_true',
-            default=default_use_ccache, help=ccache_help)
-    ccache_group.add_argument('--no-ccache', '-g', action='store_true',
-            default=(not default_use_ccache), help=no_ccache_help)
-
-    args = parser.parse_args()
-
-    if args.no_build_dir:
-        builddir = "."
-    else:
-        builddir = args.build_dir
-    """
-
     def unicode_type(utf8_string):
         return utf8_string.decode('utf8')
-
     parser = argparse.ArgumentParser(description="Manage words that will be tested.")
     parser.add_argument('--add-word', '-a', action='store', nargs=2, metavar=('KANJI', 'KANA'),
             type=unicode_type, help="fetch files for word")
+    parser.add_argument('--sanity-check', '-s', action='store_true',
+            help="run a sanity check to make sure our words.db and actual files match up")
 
     args = parser.parse_args()
 
+    if args.sanity_check:
+        sanity_check()
+        sys.exit(0)
     if args.add_word:
+        sanity_check()
         addword(args.add_word[0], args.add_word[1])
+        sys.exit(0)
 
 if __name__ == '__main__':
     main()
