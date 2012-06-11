fff435b906982fb39f73728f980fe00de3be2765
(cdep) illabout <cdep.illabout@gmail.com>
2012-06-11 09:24:51 -0400

    Added example sentences to qwebview dic output.

--- ui/defscreenui.py
+++ ui/defscreenui.py
@@ -16,14 +16,29 @@ class DefWebView(QtWebKit.QWebView):
             html = f.read().decode('utf8')
         self.defs = defs
         for i, d in enumerate(defs):
-            html += u"\n<p>%s " % unichr(ordinal + i)
+            # add definitions
+            html += u"\n<div class='definition'>%s " % unichr(ordinal + i)
             for j, p in enumerate(d.parts):
                 idattr = u"id='defpart_%s_%s'" % (i, j)
-                classattr = u"class='notselected'"
+                classattr = u"class='defpart_notselected'"
                 onclickattr = u"onclick='changeBackgroundColor(\"defpart_%s_%s\");'" % (i, j)
                 html += u'\n<span %s %s %s>%s</span>。' % \
                         (idattr, classattr, onclickattr, p.part)
-            html += u"</p>"
+            html += u"</div>"
+
+            # add example sentences
+            html += u"\n<div class='ex_sent_list'>"
+            for j, e in enumerate(d.example_sentences):
+                idattr = u"id='ex_sent_%s_%s'" % (i, j)
+                classattr = u"class='ex_sent_notselected'"
+                onclickattr = u"onclick='changeBackgroundColor(\"ex_sent_%s_%s\");'" % (i, j)
+                html += u'\n<div %s %s %s><span class="jap_sentence">%s</span>' % \
+                        (idattr, classattr, onclickattr, e.jap_sentence)
+                if e.eng_trans:
+                    html += u'<br/><span class="eng_trans">%s</span>' % e.eng_trans
+                html += u'</div>'
+            html += u"</div>"
+
         html += u"\n</body></html>"
 
         self.setHtml(html)
